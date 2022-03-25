023be8bc8d182dde381267e456afec1b68ea76ee
Dijkkla <11039lodewijk@gmail.com>
2022-03-25 09:01:19 +0100

    Added paladinsprite simple movementlistner to change sprite

--- src/main/java/com/github/hanyaeger/SlimeGame/entities/PaladinSprite.java
+++ src/main/java/com/github/hanyaeger/SlimeGame/entities/PaladinSprite.java
@@ -1,13 +1,32 @@
 package com.github.hanyaeger.SlimeGame.entities;
 
-import com.github.hanyaeger.api.Coordinate2D;
-import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import java.util.Set;
 
-public class PaladinSprite extends DynamicSpriteEntity {
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.userinput.KeyListener;
+
+import javafx.scene.input.KeyCode;
+
+public class PaladinSprite extends DynamicSpriteEntity implements KeyListener {
 
 	public PaladinSprite(Coordinate2D initialLocation) {
-		super("sprites/LifeformSpriteSheet.png", initialLocation);
-		// TODO Auto-generated constructor stub
+		super("sprites/LifeformSpriteSheet.png", initialLocation, new Size(32, 32), 8, 8);
+        setCurrentFrameIndex(0);
+	}
+
+	@Override
+	public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+		if(pressedKeys.contains(KeyCode.LEFT)) {
+	        setCurrentFrameIndex(16);
+		} else if (pressedKeys.contains(KeyCode.RIGHT)) {
+	        setCurrentFrameIndex(8);
+		} else if (pressedKeys.contains(KeyCode.UP)) {
+	        setCurrentFrameIndex(24);
+		} else if (pressedKeys.contains(KeyCode.DOWN)) {
+	        setCurrentFrameIndex(0);
+		}
 	}
 
 }
