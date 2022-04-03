e53959c416e3114de2ef8f05d23a70fb0851c317
TELEVISIN <jasperkooy123@gmail.com>
2022-04-03 13:41:40 +0200

    Attack Sprite Fix

--- src/main/java/com/github/hanyaeger/SlimeGame/entities/AttackSprite.java
+++ src/main/java/com/github/hanyaeger/SlimeGame/entities/AttackSprite.java
@@ -20,55 +20,28 @@ public class AttackSprite extends DynamicSpriteEntity implements MouseButtonPres
 	
 	public AttackSprite(Coordinate2D initialLocation) {
 		super("sprites/LifeformSpriteSheet.png", initialLocation, new Size(64, 64), 8, 8);
+		switch(PaladinSprite.direction) {
+	    	case 0:
+	    		setCurrentFrameIndex(27); //up
+	    		break;
+	    	case 1:
+	    		setCurrentFrameIndex(11); //right
+	    		break;
+	    	case 2:
+	    		setCurrentFrameIndex(3); //down
+	    		break;
+	    	case 3:
+	    		setCurrentFrameIndex(19); //left
+	    		break;
+	    	default:
+	    		break;
+	    }
 	}
 
 	@Override
 	public void onMouseButtonPressed(MouseButton button, Coordinate2D coordinate2d) {
 		// TODO Auto-generated method stub
-		if(button == MouseButton.PRIMARY) {
-			
-			//depending on where the player is facing, change the attack sprite
-			switch(PaladinSprite.direction) {
-	        	case 0:
-	        		setCurrentFrameIndex(27); //up
-	        		break;
-	        	case 1:
-	        		setCurrentFrameIndex(11); //right
-	        		break;
-	        	case 2:
-	        		setCurrentFrameIndex(3); //down
-	        		break;
-	        	case 3:
-	        		setCurrentFrameIndex(19); //left
-	        		break;
-	        	default:
-	        		break;
-	        }
-		}
-	}
-	
-	public void appear(Coordinate2D coordinate2d) {
-
-		//change sprite depending on where player is facing
-		//0 = up, 1 = right, 2 = down, 3 = left
-		switch(PaladinSprite.direction) {
-			case 0:
-				setCurrentFrameIndex(27); //up
-				break;
-			case 1:
-				setCurrentFrameIndex(11); //right
-				break;
-			case 2:
-				setCurrentFrameIndex(3); //down
-				break;
-			case 3:
-				setCurrentFrameIndex(19); //left
-				break;
-			default:
-				break;
-		}
-
-		setAnchorLocation(coordinate2d);
+		
 	}
 	
 
